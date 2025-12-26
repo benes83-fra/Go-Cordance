b8ec8530664b0ed06bcba2b3c6cdb3fbf528b0e6
benes83-fra <benjamin_esser@live.de>
2025-12-26 19:37:29 +0100

    updated  inspector

--- internal/editor/ui/inspector.go
+++ internal/editor/ui/inspector.go
@@ -1,201 +1,182 @@
 package ui
 
 import (
-	"fmt"
-	"sort"
-	"strconv"
-
-	"go-engine/Go-Cordance/internal/ecs"
-	"go-engine/Go-Cordance/internal/editor/registry"
 	state "go-engine/Go-Cordance/internal/editor/state"
 	"go-engine/Go-Cordance/internal/editorlink"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-// NewInspectorPanel returns an inspector container and a rebuild function.
-// The rebuild function signature is:
-//
-//	func(world *ecs.World, st *state.EditorState, hierarchy *widget.List)
-func NewInspectorPanel() (*fyne.Container, func(world *ecs.World, st *state.EditorState, hierarchy *widget.List)) {
-	cont := container.NewVBox()
-	cont.Add(widget.NewLabel("Select an entity"))
+// NewInspectorPanel returns the inspector container and a rebuild function.
+func NewInspectorPanel() (fyne.CanvasObject, func(world interface{}, st *state.EditorState, hierarchy *widget.List)) {
+	// Position entries
+	posX := widget.NewEntry()
+	posY := widget.NewEntry()
+	posZ := widget.NewEntry()
 
-	var rebuild func(world *ecs.World, st *state.EditorState, hierarchy *widget.List)
+	// Rotation entries (w,x,y,z) or whatever you prefer
+	rotW := widget.NewEntry()
+	rotX := widget.NewEntry()
+	rotY := widget.NewEntry()
+	rotZ := widget.NewEntry()
 
-	rebuild = func(world *ecs.World, st *state.EditorState, hierarchy *widget.List) {
-		cont.Objects = nil
-		cont.Refresh()
+	// Scale entries
+	scaleX := widget.NewEntry()
+	scaleY := widget.NewEntry()
+	scaleZ := widget.NewEntry()
 
-		// No selection
-		if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
-			cont.Add(widget.NewLabel("Select an entity"))
-			cont.Refresh()
-			return
-		}
+	// Layout
+	posBox := container.NewVBox(widget.NewLabel("Position"), posX, posY, posZ)
+	rotBox := container.NewVBox(widget.NewLabel("Rotation"), rotW, rotX, rotY, rotZ)
+	scaleBox := container.NewVBox(widget.NewLabel("Scale"), scaleX, scaleY, scaleZ)
 
-		selected := st.Entities[st.SelectedIndex]
-		e := world.FindByID(selected.ID)
-		if e == nil {
-			cont.Add(widget.NewLabel("Entity not found"))
-			cont.Refresh()
-			return
-		}
+	containerObj := container.NewVBox(posBox, rotBox, scaleBox)
 
-		// Header
-		cont.Add(widget.NewLabel(fmt.Sprintf("Entity %d", e.ID)))
+	// Rebuild function will be filled by the caller to capture world/st/hierarchy.
+	var rebuild func(world interface{}, st *state.EditorState, hierarchy *widget.List)
 
-		// -------------------------
-		// ADD COMPONENT SECTION
-		// -------------------------
-		cont.Add(widget.NewSeparator())
-		cont.Add(widget.NewLabel("Add Component"))
-
-		componentNames := make([]string, 0, len(registry.Components))
-		for name := range registry.Components {
-			componentNames = append(componentNames, name)
-		}
-		sort.Strings(componentNames)
-
-		addSelect := widget.NewSelect(componentNames, nil)
-		addButton := widget.NewButton("Add", func() {
-			if addSelect.Selected == "" {
-				return
-			}
-			factory := registry.Components[addSelect.Selected]
-			if factory != nil {
-				e.AddComponent(factory())
-				st.Entities = state.Global.Entities
-				if hierarchy != nil {
-					hierarchy.Refresh()
-				}
-				rebuild(world, st, hierarchy)
-			}
-		})
-
-		cont.Add(container.NewHBox(addSelect, addButton))
-
-		// -------------------------
-		// NAME COMPONENT
-		// -------------------------
-		if nComp := e.GetComponent(&ecs.Name{}); nComp != nil {
-			nameComp := nComp.(*ecs.Name)
-
-			header := container.NewHBox(
-				widget.NewLabel("Name"),
-				widget.NewButton("Remove", func() {
-					e.RemoveComponent(&ecs.Name{})
-					st.Entities = world.ListEntityInfo()
-					if hierarchy != nil {
-						hierarchy.Refresh()
-					}
-					rebuild(world, st, hierarchy)
-				}),
-			)
-			cont.Add(header)
-
-			nameEntry := widget.NewEntry()
-			nameEntry.SetText(nameComp.Value)
-			nameEntry.OnChanged = func(val string) {
-				nameComp.Value = val
-				st.Entities = world.ListEntityInfo()
-				if hierarchy != nil {
-					hierarchy.Refresh()
-				}
-			}
-			cont.Add(nameEntry)
-		}
-
-		// -------------------------
-		// TRANSFORM COMPONENT
-		// -------------------------
-		if t := e.GetTransform(); t != nil {
-			header := container.NewHBox(
-				widget.NewLabel("Transform"),
-				widget.NewButton("Remove", func() {
-					e.RemoveComponent(&ecs.Transform{})
-					rebuild(world, st, hierarchy)
-				}),
-			)
-			cont.Add(header)
-
-			x := widget.NewEntry()
-			y := widget.NewEntry()
-			z := widget.NewEntry()
-			x.SetText(fmt.Sprintf("%g", t.Position[0]))
-			y.SetText(fmt.Sprintf("%g", t.Position[1]))
-			z.SetText(fmt.Sprintf("%g", t.Position[2]))
-			x.OnChanged = func(val string) {
-				f, _ := strconv.ParseFloat(val, 32)
-				st.Entities[st.SelectedIndex].Position[0] = float32(f)
-
-				if editorlink.EditorConn != nil {
-					go editorlink.WriteSetTransform(editorlink.EditorConn, editorlink.MsgSetTransform{
-						ID:       uint64(st.Entities[st.SelectedIndex].ID), // cast to uint64
-						Position: [3]float32(st.Entities[st.SelectedIndex].Position),
-						Rotation: [4]float32(st.Entities[st.SelectedIndex].Rotation),
-						Scale:    [3]float32(st.Entities[st.SelectedIndex].Scale),
-					})
-				}
-			}
-
-			y.OnChanged = func(val string) {
-				f, _ := strconv.ParseFloat(val, 32)
-				st.Entities[st.SelectedIndex].Position[1] = float32(f)
-
-				if editorlink.EditorConn != nil {
-					go editorlink.WriteSetTransform(editorlink.EditorConn, editorlink.MsgSetTransform{
-						ID:       uint64(st.Entities[st.SelectedIndex].ID), // cast to uint64
-						Position: [3]float32(st.Entities[st.SelectedIndex].Position),
-						Rotation: [4]float32(st.Entities[st.SelectedIndex].Rotation),
-						Scale:    [3]float32(st.Entities[st.SelectedIndex].Scale),
-					})
-				}
-			}
-
-			z.OnChanged = func(val string) {
-				f, _ := strconv.ParseFloat(val, 32)
-				st.Entities[st.SelectedIndex].Position[2] = float32(f)
-
-				if editorlink.EditorConn != nil {
-					go editorlink.WriteSetTransform(editorlink.EditorConn, editorlink.MsgSetTransform{
-						ID:       uint64(st.Entities[st.SelectedIndex].ID), // cast to uint64
-						Position: [3]float32(st.Entities[st.SelectedIndex].Position),
-						Rotation: [4]float32(st.Entities[st.SelectedIndex].Rotation),
-						Scale:    [3]float32(st.Entities[st.SelectedIndex].Scale),
-					})
-				}
-			}
-
-			apply := widget.NewButton("Apply", func() {
-				if fx, err := strconv.ParseFloat(x.Text, 32); err == nil {
-					t.Position[0] = float32(fx)
-				}
-				if fy, err := strconv.ParseFloat(y.Text, 32); err == nil {
-					t.Position[1] = float32(fy)
-				}
-				if fz, err := strconv.ParseFloat(z.Text, 32); err == nil {
-					t.Position[2] = float32(fz)
-				}
-				t.Dirty = true
-			})
-
-			cont.Add(container.NewVBox(
-				container.NewHBox(widget.NewLabel("X:"), x),
-				container.NewHBox(widget.NewLabel("Y:"), y),
-				container.NewHBox(widget.NewLabel("Z:"), z),
-				apply,
-			))
-		}
-
-		// -------------------------
-		// TODO: Add more components here
-		// -------------------------
-
-		cont.Refresh()
+	// Helper to parse float safely
+	parse32 := func(s string) float32 {
+		f, _ := strconv.ParseFloat(s, 32)
+		return float32(f)
 	}
 
-	return cont, rebuild
+	// OnChanged handlers will be set inside rebuild so they capture st.SelectedIndex correctly.
+	rebuild = func(world interface{}, st *state.EditorState, hierarchy *widget.List) {
+		// Defensive: no selection
+		if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+			posX.SetText("")
+			posY.SetText("")
+			posZ.SetText("")
+			rotW.SetText("")
+			rotX.SetText("")
+			rotY.SetText("")
+			rotZ.SetText("")
+			scaleX.SetText("")
+			scaleY.SetText("")
+			scaleZ.SetText("")
+			return
+		}
+
+		ent := st.Entities[st.SelectedIndex]
+
+		// Fill UI fields from st.Entities (bridge.EntityInfo must have Position/Rotation/Scale)
+		posX.SetText(strconv.FormatFloat(float64(ent.Position[0]), 'f', 4, 32))
+		posY.SetText(strconv.FormatFloat(float64(ent.Position[1]), 'f', 4, 32))
+		posZ.SetText(strconv.FormatFloat(float64(ent.Position[2]), 'f', 4, 32))
+
+		rotW.SetText(strconv.FormatFloat(float64(ent.Rotation[0]), 'f', 4, 32))
+		rotX.SetText(strconv.FormatFloat(float64(ent.Rotation[1]), 'f', 4, 32))
+		rotY.SetText(strconv.FormatFloat(float64(ent.Rotation[2]), 'f', 4, 32))
+		rotZ.SetText(strconv.FormatFloat(float64(ent.Rotation[3]), 'f', 4, 32))
+
+		scaleX.SetText(strconv.FormatFloat(float64(ent.Scale[0]), 'f', 4, 32))
+		scaleY.SetText(strconv.FormatFloat(float64(ent.Scale[1]), 'f', 4, 32))
+		scaleZ.SetText(strconv.FormatFloat(float64(ent.Scale[2]), 'f', 4, 32))
+
+		// Set OnChanged handlers to update state and send SetTransform
+		posX.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Position[0] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		posY.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Position[1] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		posZ.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Position[2] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+
+		// Rotation handlers (same pattern)
+		rotW.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Rotation[0] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		rotX.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Rotation[1] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		rotY.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Rotation[2] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		rotZ.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Rotation[3] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+
+		// Scale handlers
+		scaleX.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Scale[0] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		scaleY.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Scale[1] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+		scaleZ.OnChanged = func(val string) {
+			if st.SelectedIndex < 0 || st.SelectedIndex >= len(st.Entities) {
+				return
+			}
+			st.Entities[st.SelectedIndex].Scale[2] = parse32(val)
+			sendTransformIfConnected(st, st.SelectedIndex)
+		}
+	}
+
+	// Return the UI and the rebuild function
+	return containerObj, func(world interface{}, st *state.EditorState, hierarchy *widget.List) {
+		rebuild(world, st, hierarchy)
+	}
+}
+
+// helper to send transform
+func sendTransformIfConnected(st *state.EditorState, idx int) {
+	if editorlink.EditorConn == nil {
+		return
+	}
+	ent := st.Entities[idx]
+	// Build message with proper casts
+	msg := editorlink.MsgSetTransform{
+		ID:       uint64(ent.ID),
+		Position: [3]float32{ent.Position[0], ent.Position[1], ent.Position[2]},
+		Rotation: [4]float32{ent.Rotation[0], ent.Rotation[1], ent.Rotation[2], ent.Rotation[3]},
+		Scale:    [3]float32{ent.Scale[0], ent.Scale[1], ent.Scale[2]},
+	}
+	go func(m editorlink.MsgSetTransform) {
+		if err := editorlink.WriteSetTransform(editorlink.EditorConn, m); err != nil {
+			// optional: log error
+			// log.Printf("editor: WriteSetTransform error: %v", err)
+		}
+	}(msg)
 }
